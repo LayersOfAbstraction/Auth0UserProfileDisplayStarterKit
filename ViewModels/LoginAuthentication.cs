c4c9d9f771c159eef6a844442a373170a0fe9957
LayersOfAbstraction <jnash51@live.com>
2021-09-13 06:46:40 +1000

    fully abstract AccessTokenManagement:Audience

--- ViewModels/LoginAuthentication.cs
+++ ViewModels/LoginAuthentication.cs
@@ -1,17 +1,26 @@
+using System.IO;
 using Auth0.AuthenticationApi;
-
+using Microsoft.Extensions.Configuration;
 namespace Auth0UserProfileDisplayStarterKit.ViewModels
 {
     public class LoginAuthentication
     {
-        public static Auth0Token Login(string ClientID, string ClientSecret, string domain)
+      public static IConfiguration AppSetting { get; }
+        static LoginAuthentication()
+        {
+            AppSetting = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+        }
+        public static Auth0Token Login(string ClientID, string ClientSecret, string domain, string Audience)
         {
             var authenticationApiClient = new AuthenticationApiClient(domain);
             var token =  authenticationApiClient.GetTokenAsync(new Auth0.AuthenticationApi.Models.ClientCredentialsTokenRequest
             {                
                 ClientId = ClientID,
                 ClientSecret = ClientSecret,
-                Audience = "https://dev-dgdfgfdgf324.au.auth0.com/api/v2/"
+                Audience = LoginAuthentication.AppSetting["AccessTokenManagement:Audience"],
             }).Result;
             return new Auth0Token {strAuth0Token = token.AccessToken};   
         }
