e1d35cae36e22f24d071c00cd7de393664ed0a01
LayersOfAbstraction <jnash51@live.com>
2021-10-02 08:45:34 +1000

    add db creation logic

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Auth0UserProfileDisplayStarterKit.Data;
+using Microsoft.Extensions.Logging;
 
 namespace SampleMvcApp
 {
@@ -7,7 +11,28 @@ namespace SampleMvcApp
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            CreateDbIfNotExists(host);
+
+            host.Run();
+        }
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<TeamContext>();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
