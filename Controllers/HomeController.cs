bab123a57be3e2cf167554e0a42234fcf36bcf0c
Jordan Nash <jnash51@live.com>
2023-02-12 11:48:11 +1000

    Make domain private.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
 
 namespace Auth0UserProfileDisplayStarterKit.Controllers
 {
@@ -12,6 +15,21 @@ namespace Auth0UserProfileDisplayStarterKit.Controllers
         public IActionResult Error()
         {
             return View();
+        }
+        public async Task<IActionResult> GetAllAuth0Users()
+        {
+            //Get token
+            var apiClient = new ManagementApiClient(ConstantStrings.strToken,
+                new Uri("{DOMAIN}"));
+            //Get Auth0 Users
+            var allUsers = await apiClient.Users.GetAllAsync(new Auth0.ManagementApi.Models.GetUsersRequest(), new Auth0.ManagementApi.Paging.PaginationInfo());
+            var renderedUsers = allUsers.Select(u => new Auth0UserProfileDisplayStarterKit.ViewModels.User
+            {
+                UserFirstName = u.FullName.Contains(' ') ? u.FullName.Split(' ')[0] : "no space",
+                UserLastName = u.FullName.Contains(' ') ? u.FullName.Split(' ')[1] : "no space",
+                UserContactEmail = u.Email
+            }).ToList();
+            return Json(renderedUsers);
         }
     }
 }
