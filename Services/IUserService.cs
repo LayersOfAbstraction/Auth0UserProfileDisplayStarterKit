07cf050e0aa48ac460d98858dc4eb67f696e2d49
Jordan Nash <jnash51@live.com>
2023-03-12 00:29:55 +1000

    Resolve conflict User class with dependencies and delete namespace.

--- Services/IUserService.cs
+++ Services/IUserService.cs
@@ -1,13 +1,15 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 using Auth0.ManagementApi.Models;
-using Auth0.ManagementApi.Paging;
+using Auth0.ManagementApi;
 
-namespace Example.Auth0.AuthenticationApi.Services
-{
+//The namespace is different to the rest of the project
+//and stops the ManagementApi from been recognized.
+//It has been deleted.
     public interface IUserService
     {
-        Task<IPagedList<User>> GetUsersAsync(GetUsersRequest request, PaginationInfo paginationInfo,
-            CancellationToken cancellationToken);
+        //Prepending Auth0.ManagementApi.Models to User prevents confusion
+        //for .NET 6 to know if we want to list Auth0 users or our ownn view model users.
+        Task<IPagedList<Auth0.ManagementApi.Models.User>> GetUsersAsync(GetUsersRequest request, PaginationInfo paginationInfo,
+                CancellationToken cancellationToken);
     }
-}
