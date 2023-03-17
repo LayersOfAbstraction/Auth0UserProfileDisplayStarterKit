779942dd465068f9c9ca1172ae4283885b0e8578
Jordan Nash <jnash51@live.com>
2023-03-17 22:09:56 +1000

    Add cancellationToken to allUsers.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,6 +7,14 @@ namespace Auth0UserProfileDisplayStarterKit.Controllers
 {
     public class HomeController : Controller
     {
+        public IPagedList<Auth0.ManagementApi.Models.User> Users { get; private set; }
+        private readonly IUserService _userService;
+
+        public HomeController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        
         public IActionResult Index()
         {
             return View();
@@ -16,13 +24,11 @@ namespace Auth0UserProfileDisplayStarterKit.Controllers
         {
             return View();
         }
-        public async Task<IActionResult> GetAllAuth0Users()
+
+        public async Task<IActionResult> GetAllAuth0Users(CancellationToken cancellationToken)
         {
-            //Get token
-            var apiClient = new ManagementApiClient(ConstantStrings.strToken,
-                new Uri("https://dev-dgdfgfdgf324.au.auth0.com/api/v2/"));
-            //Get Auth0 Users
-            var allUsers = await apiClient.Users.GetAllAsync(new Auth0.ManagementApi.Models.GetUsersRequest(), new Auth0.ManagementApi.Paging.PaginationInfo());
+            //Get new token using Auth0ManagementApi with IUserService to interface with Identity Model library.
+            var allUsers = await _userService.GetUsersAsync(new GetUsersRequest(), new PaginationInfo(), cancellationToken);
             var renderedUsers = allUsers.Select(u => new Auth0UserProfileDisplayStarterKit.ViewModels.User
             {
                 UserFirstName = u.FullName.Contains(' ') ? u.FullName.Split(' ')[0] : "no space",
@@ -30,6 +36,11 @@ namespace Auth0UserProfileDisplayStarterKit.Controllers
                 UserContactEmail = u.Email
             }).ToList();
             return Json(renderedUsers);
+        }
+
+        public async Task OnGet(CancellationToken cancellationToken)
+        {
+            Users = await _userService.GetUsersAsync(new GetUsersRequest(), new PaginationInfo(), cancellationToken);
         }
     }
 }
