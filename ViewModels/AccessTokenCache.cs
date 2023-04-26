12fcb37edb9f2eb6964389c13d85f51dc949ea7c
LayersOfAbstraction <jnash51@live.com>
2023-04-26 18:42:01 +1000

    Add new class.

---
+++ ViewModels/AccessTokenCache.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SampleMvcApp.ViewModels
+{ 
+    internal partial class AccessTokenCache
+    {
+        public string Id { get; set; }
+        public byte[] Value { get; set; }
+        public DateTimeOffset ExpiresAtTime { get; set; }
+        public long? SlidingExpirationInSeconds { get; set; }
+        public DateTimeOffset? AbsoluteExpiration { get; set; }
+    }
+}
+
