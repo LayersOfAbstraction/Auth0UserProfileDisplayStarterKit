c23ec724593c7211b4d0de8b2627f6be1eb773b9
Jordan Nash <jnash51@live.com>
2023-04-27 19:56:41 +1000

    Add new class.

---
+++ ViewModels/AccessTokenCache.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Auth0UserProfileDisplayStarterKit.ViewModels
+{
+    internal partial class AccessTokenCache
+    {
+        public string Id { get; set; }
+        public byte[] Value { get; set; }
+        public DateTimeOffset ExpiresAtTime { get; set; }
+        public long? SlidingExpirationInSeconds { get; set; }
+        public DateTimeOffset? AbsoluteExpiration { get; set; }
+    }
+
+}
